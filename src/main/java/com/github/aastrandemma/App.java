b49cb056516367712e199ff6f54b70984a994c8f
Emma Åstrand <aastrand.emma@gmail.com>
2024-03-14 13:39:14 +0100

    Feat: Part 3 workshop (findByFirstName, findByLastName and update methods)

--- src/main/java/com/github/aastrandemma/App.java
+++ src/main/java/com/github/aastrandemma/App.java
@@ -8,5 +8,61 @@ public class App {
         System.out.println(NameRepository.getSize());
         String foundTestName = NameRepository.find("test test");
         System.out.println("foundTestName = " + foundTestName);
+        System.out.println();
+
+        // Part 3
+        partThreeOne();
+        partThreeTwo();
+        partThreeThree();
+    }
+
+    private static void partThreeOne() {
+        addFirstNamesForPartThree();
+        //String[] foundAllByFirstName = NameRepository.findByFirstName("Jane"); // Matches 3
+        String[] foundAllByFirstName = NameRepository.findByFirstName("Emma"); // No matches
+        System.out.println();
+        if (foundAllByFirstName.length >= 1) {
+            for (String foundFirstName : foundAllByFirstName) {
+                System.out.println("foundAllByFirstName = " + foundFirstName);
+            }
+        } else {
+            System.out.println("No matches found by first name.");
+        }
+    }
+
+    private static void partThreeTwo() {
+        addLastNamesForPartThree();
+        String[] foundAllByLastName = NameRepository.findByLastName("Doe"); // Matches 4
+        //String[] foundAllByLastName = NameRepository.findByLastName("Test"); // No matches
+        System.out.println();
+        if (foundAllByLastName.length >= 1) {
+            for (String foundLastName : foundAllByLastName) {
+                System.out.println("foundLastName = " + foundLastName);
+            }
+        } else {
+            System.out.println("No matches found by last name.");
+        }
+    }
+
+    private static void partThreeThree() {
+        boolean updatedFullName = NameRepository.update("Test Testsson", "Joe Schmo"); // Matches
+        //boolean updatedFullName = NameRepository.update("Schmo Test", "Joe Schmo"); // False = original don't exist
+        //boolean updatedFullName = NameRepository.update("Test Testsson", "Jane Doe"); // False = updateName exist
+        if (updatedFullName) {
+            System.out.println();
+            System.out.println("updatedFullName = " + updatedFullName);
+        }
+    }
+
+    private static void addFirstNamesForPartThree() {
+        boolean isAddedTestNameOne = NameRepository.add("Jane Doe");
+        boolean isAddedTestNameTwo = NameRepository.add("jane Harry");
+        boolean isAddedTestNameThree = NameRepository.add("Jane Tom");
+    }
+
+    private static void addLastNamesForPartThree() {
+        boolean isAddedTestNameOne = NameRepository.add("Harry Doe");
+        boolean isAddedTestNameTwo = NameRepository.add("Tom doe");
+        boolean isAddedTestNameThree = NameRepository.add("John Doe");
     }
 }
